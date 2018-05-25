384480092a31ebfd5af0851206255163b03584d0
gracetikaa <gtika97@gmail.com>
2018-05-25 11:06:39 +0700

    matchers

--- Tugas 1/src/test/scala/FileClassTest.scala
+++ Tugas 1/src/test/scala/FileClassTest.scala
@@ -1,7 +1,7 @@
 import org.scalatest._
 import Matchers._
 
-class FileClassTest extends FunSuite {
+class FileClassTest extends FunSuite with Matchers{
   val obj = new File
   val a = obj.open("1.txt")
   a should be ("List(ini file teks)")
--- Tugas 2/src/test/scala/UrlTest.scala
+++ Tugas 2/src/test/scala/UrlTest.scala
@@ -1,7 +1,7 @@
 import org.scalatest._
 import Matchers._
 
-class UrlTest extends FunSuite {
+class UrlTest extends FunSuite with Matchers{
   val obj = new Url
   val a = obj.JsonParse("https://sport.detik.com/sepakbola/uefa/d-4036390/road-to-final-liga-champions-20172018-real-madrid").toString()
   a should not be empty
